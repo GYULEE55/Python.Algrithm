f26d51df07154f32091df797da77422a8e409a11
Seunggyu LEE <jin05005@naver.com>
2026-02-20 15:24:33 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 49.78 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,10 @@
+from collections import Counter
+def solution(participant, completion):
+    counter = Counter(participant)  # {'mislav': 2, 'stanko': 1, 'ana': 1}
+    
+    for name in completion:
+        counter[name] -= 1          # 완주한 사람 하나씩 빼기
+    
+    for name, count in counter.items():
+        if count > 0:               # 안 빠진 사람 = 미완주
+            return name
