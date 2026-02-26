bdb9aded893027540729372d9b2d349c1a069cc0
Seunggyu LEE <jin05005@naver.com>
2026-02-27 03:09:54 +0900

    Fix : 오류

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -6,5 +6,5 @@ def solution(participant, completion):
         counter[name] -= 1          # 완주한 사람 하나씩 빼기
     
     for name, count in counter.items():
-        if count > 0:               # 안 빠진 사람 = 미완주
+        if count > 0:              
             return name
