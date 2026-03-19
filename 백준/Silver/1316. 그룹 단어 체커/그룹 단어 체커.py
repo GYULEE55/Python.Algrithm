44aeea79d808805419e512bcb7bdc80b5371c2f2
Seunggyu LEE <jin05005@naver.com>
2026-03-19 11:51:30 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,22 @@
+n = int(input())
+count = 0
+
+for _ in range(n):
+    word = input().strip()
+    seen = set()
+    prev = ''
+
+    is_group = True
+
+    for ch in word:
+        if ch != prev:
+            if ch in seen:
+                is_group = False
+                break
+            seen.add(ch)
+        prev = ch
+
+    if is_group:
+        count += 1
+
+print(count)
